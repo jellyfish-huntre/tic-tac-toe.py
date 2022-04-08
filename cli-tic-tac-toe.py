09ec6d340b43333f5acb80e6ea9c9c10ad259cf5
Huntree <75651687+jellyfish-huntre@users.noreply.github.com>
2022-04-08 15:16:05 -0400

    Update cli-tic-tac-toe.py

--- cli-tic-tac-toe.py
+++ cli-tic-tac-toe.py
@@ -1,29 +1,27 @@
 import math
 
-board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
-# list(range(1, 10))  # [1, 2, 3, 4, 5, 6, 7, 8, 9]
-moves = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']] # board of rows of tiles
+moves = [1, 2, 3, 4, 5, 6, 7, 8, 9] # available moves
 
-
-def main():
+def main(): # main function, this is the equivalent of one tic-tac-toe game
     player = 'x'
-    printBoard()
-    winner = whoWon()
-    while winner == None:
+    
+    while whoWon() == None: # while the game has not ended
         move(player)
         if player == 'o':
             player = 'x'
         else:
             player = 'o'
         winner = whoWon()
-
+    
+    printBoard()
     if winner != 'draw':
         print(winner + ' wins!')
     else:
         print('Draw!')
 
-
 def printBoard():
+    '''prints the board as output'''
     for row in board:
         print(row[0] + ' | ' + row[1] + ' | ' + row[2])
         if board[2] is not row:
@@ -31,33 +29,45 @@ def printBoard():
 
 
 def promptUser(prompt: str, errorPrompt: str, choices: list, responseType=str):
-    userInput = responseType(input(prompt))
+    '''continously prompts the user until the user inputs an option. 
+    Takes the original prompt, an error prompt, the choices, and the response type. Returns the input.'''
+    userInput = input(prompt)
+    try:
+        userInput = responseType(userInput)
+    except:
+        pass
     while (userInput not in choices):
-        userInput = responseType(input(errorPrompt))
+        userInput = input(errorPrompt)
+        try:
+            userInput = responseType(userInput)
+        except:
+            pass
 
     return userInput
 
 
 def move(player: str):
+    '''Takes the player as a parameter, prompts the user for their move, and changes the board'''
     print(player + "'s turn")
+    printBoard()
     move = promptUser("What is your move? (1-9) ",
-                      "Please enter an integer between 1 and 9 that has not been taken: ", moves, int)
+                      "Please enter a number between 1 and 9 that has not been taken: ", moves, int)
     moves.remove(move)
     changeBoard(move, player)
 
 
 def changeBoard(move, player):
+    '''accesses the board and changes one of the tiles. Takes a move and a player as parameters.'''
     move -= 1
     row = math.floor(move/3)
     tile = move % 3
     board[row][tile] = player
-    printBoard()
+    print("\n")
 
 
 def whoWon():
-    # for row in board:
-    #     if row[0] != ' ' and row[0] == row[1] and row[1] == row[2]:
-    #         return row[0]
+    '''Checks if the game is over, and returns draw or the player who won. 
+    Returns none if the game is still ongoing.'''
 
     tiles = []
 
@@ -78,6 +88,8 @@ def whoWon():
 
 
 def isFull():
+    '''Checks each tile in the board and checks if they are empty. 
+    If none are empty, return True. Else, return false.'''
     for row in board:
         for tile in row:
             if tile == ' ':
@@ -85,4 +97,17 @@ def isFull():
     return True
 
 
-main()
+'''this is the mainloop. the user plays a game of tic tac toe and can choose to quit or play another, 
+in which everything resets.'''
+
+play = 'y'
+print("Welcome to Tic-Tac-Toe!")
+
+while play != 'n':
+    main()
+    play = promptUser('Would you like to play again? (y/n) ', "Please type 'y' or 'n': ", ['y', 'n'])
+    print("\n")
+    board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+    moves = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print("Thank you for playing!")
